19672a1728bbddf720b155df7ec9b8d8d53a4baa
Vipin <kumar.vipin1v00@gmail.com>
2022-01-09 01:17:38 +0530

    .
    .

---
+++ numpy.py
@@ -0,0 +1,9 @@
+import pandas_custom as pdc
+
+a = 30
+b = 40
+
+s = pdc.sum(a,b)
+print(s)
+
+# test this for git with pycharm
---
+++ pandas_custom.py
@@ -0,0 +1,8 @@
+print("hi")
+a= 3
+b =3
+def sum(a,b):
+    return a+b
+
+s = sum(a,b)
+print(s)
